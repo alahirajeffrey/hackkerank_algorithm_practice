a9cb05d167e54051483608244f18b2553c38562b
Alahira Jeffrey Calvin <alahirajeffrey@gmail.com>
2021-11-14 19:33:29 +0100

    completed camelcase algorithm

---
+++ CamelCase.py
@@ -0,0 +1,40 @@
+"""
+visit https://www.hackerrank.com/challenges/camelcase/problem?isFullScreen=true for details
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    # Write your code here
+    n = 1
+    
+    for i in s:
+        if i.isupper():
+            n += 1
+    
+    return n
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
