6b61bf28fe26e3de7d7a4e5f86a2e4f5db546e70
Alahira Jeffrey Calvin <alahirajeffrey@gmail.com>
2022-01-03 14:46:23 +0100

    completed jumping on clouds challenge

---
+++ jumping_on_clouds.py
@@ -0,0 +1,41 @@
+# visit https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+# to view the challenge
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    total_jumps = 0
+    local_safe_clouds = 0
+    for i in c:
+        if i:
+            total_jumps += 1 + local_safe_clouds // 2
+            local_safe_clouds = 0
+        else:
+            local_safe_clouds+=1
+    return total_jumps + local_safe_clouds // 2
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
